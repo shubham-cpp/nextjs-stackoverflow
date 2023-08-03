74226acf801bfa47bb8256d6c1c1baceb10ce499
Shubham Pawar <shubhampawar3007@gmail.com>
2023-08-03 20:44:29 +0530

    different width for mobile and desktop

--- components/ProfileCards.tsx
+++ components/ProfileCards.tsx
@@ -1,5 +1,5 @@
 const ProfileCards = () => {
-  return <div className="w-60 bg-gray-100 h-28"></div>;
+  return <div className="w-[calc(100vw_-_3rem)] md:w-60 bg-gray-100 h-28"></div>;
 };
 
 export default ProfileCards;
--- components/TagsCard.tsx
+++ components/TagsCard.tsx
@@ -1,6 +1,6 @@
 const TagsCard = () => {
   return (
-    <div className="w-[18rem] h-[12rem] bg-slate-50 rounded border-2 dark:bg-zinc-900 dark:border-grey-400"></div>
+    <div className="w-[calc(100vw_-_3rem)] md:w-60 h-[12rem] bg-slate-50 rounded border-2 dark:bg-zinc-900 dark:border-grey-400"></div>
   );
 };
 
